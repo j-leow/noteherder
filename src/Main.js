a1000f48fb19b2e3db22f9549c37ea03b81cf696
Jules Leow <jleow@iupui.edu>
2017-06-14 14:03:27 -0400

    Make app look like static.html by adding CSS.

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './css/Main.css'
+
 import NoteForm from './NoteForm'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,7 +3,18 @@ import './css/NoteForm.css'
 
 class NoteForm extends Component {
   render() {
-    return (<p>FORM!</p>)
+    return (
+    <div className="NoteForm">
+        <form>
+          <p>
+            <input type="text" name="title" placeholder="Title your note" value="" />
+          </p>
+          <p>
+            <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+          </p>
+        </form>
+      </div>
+    )
   }
 }
 
