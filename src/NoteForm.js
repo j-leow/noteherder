658f3dbd88bf7ce2c44f128278a1cb490c019eac
Jules Leow <jleow@iupui.edu>
2017-06-14 14:52:00 -0400

    Create constructor, and updateNote function.

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react'
 import './css/NoteForm.css'
 
 class NoteForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      noteTitle: '',
+      noteText: '',
+      noteTitles: [],
+      noteTexts: [],
+    }
+
+    this.updateNote = this.updateNote.bind(this)
+  }
+
+  updateNote(ev) {
+    this.setState({
+      noteTitle: ev.target.value,
+      noteText: ev.target.value
+    })
+  }
+
   render() {
-    return (
+   return (
     <div className="NoteForm">
         <form>
           <p>
-            <input type="text" name="title" placeholder="Title your note" value="" />
+            <input 
+              type="text" 
+              name="title" 
+              placeholder="Title your note" 
+              value={this.state.noteTitle}
+              onChange={this.updateNote} />
           </p>
           <p>
-            <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+            <textarea 
+              name="body" 
+              cols="30" 
+              rows="10" 
+              placeholder="Just start typing..."
+              onChange={this.updateNote}>
+            </textarea>
           </p>
+          <button className="button" onClick={this.addNote}>
+            Submit
+          </button>
         </form>
       </div>
     )
