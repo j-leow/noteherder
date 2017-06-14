1385026b855f9119e0c1a82b12bec87c9766c44a
Jules Leow <jleow@iupui.edu>
2017-06-14 14:50:21 -0400

    Map information in NoteList

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,47 +1,48 @@
 import React from 'react'
 import './css/NoteList.css'
 
+const noteData = [
+  {
+    noteTitle: "Citizens of distant epochs",
+    noteBody: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?",
+  },
+  {
+    noteTitle: 'Preserve and cherish that pale blue dot',
+    noteBody: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!",
+  },
+  {
+    noteTitle: 'Laws of physics',
+    noteBody: 'Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas',
+  },
+]
+
 const NoteList = () => {
   return (
     <div className="NoteList">
       <h3>Notes</h3>
-      <ul id="notes">
-        <li>
+      
           <div className="note">
-            <div className="note-title">
-              Citizens of distant epochs
-            </div>
-            <div className="note-body">
-              <p>
-                Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
-              </p>
-            </div>
+            <ul id="notes">
+            {noteData.map((note, i) => <NoteLink key={i} note={note} />)}
+            </ul>
           </div>
-        </li>
-
-        <li>
-            <div className="note">
-              <div className="note-title">Preserve and cherish that pale blue dot </div>
-              <div className="note-body">
-                <p>
-                  network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
-                </p>
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div className="note">
-              <div className="note-title">Laws of physics</div>
-              <div className="note-body">
-                <p>
-                  Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
-                </p>
-              </div>
-            </div>
-          </li>
-      </ul>
+        
     </div>
+  )
+}
+
+function NoteLink(props) {
+  return (
+      <li>
+        <div className="note-title">
+          {props.note.noteTitle}
+        </div>
+        <div className="note-body">
+          <p>
+            {props.note.noteBody}
+          </p>
+        </div>
+      </li>
   )
 }
 
